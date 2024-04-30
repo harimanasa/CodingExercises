3734208640deaf727181701de6ce3b80b6f22ad4
harimanasa <harimanasa17@gmail.com>
2024-04-30 16:15:36 -0700

    TwoPointers_SortedSquares_Of_SortedArray.java

---
+++ CodingExercises/All_coding_problems/SortedSquares_Of_SortedArray.java
@@ -0,0 +1,30 @@
+/**
+ * I/P = [-2, -1, 0, 2, 3]
+ * 
+ * O/P = [0, 1, 4, 4, 9]
+ */
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        
+        int left = 0, right = n-1;
+        int outputIndex = n-1;
+
+        while(left<=right){
+            int leftSquare = nums[left]*nums[left];
+            int rightSquare = nums[right]*nums[right];
+
+            if(leftSquare>rightSquare){
+                output[outputIndex--] = leftSquare;
+                left++;
+            } else{
+                output[outputIndex--] = rightSquare;
+                right--;
+            }
+        }
+
+        return output;
+    }
+}
